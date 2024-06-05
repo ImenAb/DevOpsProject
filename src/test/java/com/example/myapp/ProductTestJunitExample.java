5a34f1897eedf0b1dda54baf602eaa901f194100
zeinebchebbi123 <chebbbi.zeineb@esprit.tn>
2024-06-05 11:01:56 +0100

    test

---
+++ src/test/java/com/example/myapp/ProductTestJunitExample.java
@@ -0,0 +1,20 @@
+package com.example.myapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductTestJunitExample {
+
+
+
+//        @Autowired
+//        private MyService myService;
+//
+//        @Test
+//        public void testServiceMethod() {
+//            String result = myService.someMethod();
+//            assertThat(result).isEqualTo("expectedValue");
+//        }
+
+}
